ac973ca6603c0a1c08c7e39d4e80a45b569550a2
Oliver Nguyen <nkhanhvan293@gmail.com>
2024-09-10 23:56:57 +0700

    initial

---
+++ main.js
@@ -0,0 +1,31 @@
+var countDownDate = new Date("Oct 25, 2024 17:00:00").getTime();
+
+var x = setInterval(function() {
+
+//   Computation 
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  
+//   Convert
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+//   One liner  
+//   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+//   + minutes + "m " + seconds + "s ";
+
+//   Render
+  document.getElementById("day").innerHTML = days;  
+  document.getElementById("hr").innerHTML = hours;
+  document.getElementById("min").innerHTML = minutes;
+  document.getElementById("sec").innerHTML = seconds;
+
+//   Error message
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("demo").innerHTML = "NGHI GOI";
+//   }
+}, 1000);
